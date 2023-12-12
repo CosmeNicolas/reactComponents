3a4aa8d97c8ab37e74684b822ee8e1c1428f027a
CosmeNicolas <nicomas7590@gmail.com>
2023-12-12 18:06:41 -0300

    desestructuramos el obejto props en el componente informacion

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,14 +1,14 @@
 
 
-const Informacion = (props) => {
+const Informacion = ({comision, anioActualprops}) => {
   //aqui va la lágica del componente
   return (
     //aqui puede ir un poco de lógica
     <>
-      <h2>Conceptos de React {props.comision}</h2>
+      <h2>Conceptos de React {comision}</h2>
     <section className="container">
       {/* aqui va todo el maquetado del componente */}
-      <p>React es una biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables. Desarrollada por Facebook, React se centra en la creación de componentes individuales que representan partes específicas de una interfaz de usuario. Estos componentes se pueden combinar para formar interfaces complejas.<br/> comision: {props.anioActualprops}
+      <p>React es una biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables. Desarrollada por Facebook, React se centra en la creación de componentes individuales que representan partes específicas de una interfaz de usuario. Estos componentes se pueden combinar para formar interfaces complejas.<br/> comision: {anioActualprops}
 
       </p>
     </section>
