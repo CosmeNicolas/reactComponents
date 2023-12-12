88a23fd1e6d1136750515e2c8842baea384bbac0
CosmeNicolas <nicomas7590@gmail.com>
2023-12-12 18:01:50 -0300

    agregamos props a la rama

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   //const [count, setCount] = useState(0)
-
+  const  anioActual = 2023
   return (
     <section>
       <h1 className="display-3 text-center">Proyecto de React</h1>
-      <Informacion/>
+      <Informacion comision='c76i' anioActualprops={anioActual}/>
       <Informacion/>
     </section>
   )
--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,14 +1,16 @@
 
 
-const Informacion = () => {
+const Informacion = (props) => {
   //aqui va la lágica del componente
   return (
     //aqui puede ir un poco de lógica
     <>
-    <section>
-      <h2>Conceptos de React</h2>
+      <h2>Conceptos de React {props.comision}</h2>
+    <section className="container">
       {/* aqui va todo el maquetado del componente */}
-      <p>React es una biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables. Desarrollada por Facebook, React se centra en la creación de componentes individuales que representan partes específicas de una interfaz de usuario. Estos componentes se pueden combinar para formar interfaces complejas.</p>
+      <p>React es una biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables. Desarrollada por Facebook, React se centra en la creación de componentes individuales que representan partes específicas de una interfaz de usuario. Estos componentes se pueden combinar para formar interfaces complejas.<br/> comision: {props.anioActualprops}
+
+      </p>
     </section>
     </>
   );
