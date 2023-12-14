b22e27394444f98498e4b7bb77470c18e38fd496
CosmeNicolas <nicomas7590@gmail.com>
2023-12-14 16:46:00 -0300

    actualizamos el contador  usando setNumero

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,10 +1,22 @@
+//crear estados , "use"
+import { useState } from "react";
+
 const Contador = () => {
+  /* agregar lógica */
+  const [numero, setNumero] = useState(10)
+  //let numero = 10;
+
+
+
   return (
     <div>
       <article className="text-center">
         <h2>Contador</h2>
-        <p>10</p>
-        <button className="btn btn-primary">+1</button>
+        <p>{numero}</p>
+        <button className="btn btn-primary" 
+        onClick={()=> setNumero(numero +1 )}>
+          +1
+          </button>
       </article>
     </div>
   );
