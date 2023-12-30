366c375781f7b925223bf18c0f5274f646ab3acb
CosmeNicolas <nicomas7590@gmail.com>
2023-12-29 21:02:30 -0300

    cenetre el titulo de la informacion

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -5,7 +5,7 @@ const Informacion = ({comision, anioActualprops}) => {
   return (
     //aqui puede ir un poco de lógica
     <>
-      <h2>Conceptos de React {comision}</h2>
+      <h2 className="text-center">Conceptos de React {comision}</h2>
     <section className="container">
       {/* aqui va todo el maquetado del componente */}
       <p>React es una biblioteca de JavaScript utilizada para construir interfaces de usuario interactivas y reutilizables. Desarrollada por Facebook, React se centra en la creación de componentes individuales que representan partes específicas de una interfaz de usuario. Estos componentes se pueden combinar para formar interfaces complejas.<br/> comision: {anioActualprops}
